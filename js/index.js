5f89e515e29fb36d1447fb50253688ac29dea498
semagul <isemagul@gmail.com>
2022-05-31 01:30:22 +0200

    3.3 completed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-let hacker1 = "suzy";
+let hacker1 = "evangelo";
 console.log(`The driver's name is ${hacker1}.`)
 
-let hacker2 = "jojel";
+let hacker2 = "lokumi";
 console.log(`The navigator's name is ${hacker2}.`)
 
 // Iteration 2: Conditionals
@@ -19,13 +19,36 @@ let upperCase = "";
 for (let i = 0; i < hacker1.length; i++) {
    upperCase += hacker1[i].toUpperCase() + " ";
 }
-console.log(upperCase);
+upperCase=upperCase.trimEnd()
+
 
 function reverseString(s){
     return s.split("").reverse().join("");
 }
 
-reverseString(upperCase);
+upperCase = reverseString(upperCase);
+console.log(upperCase);
+
+/* my initial atttemp
+if (hacker1 > hacker2) {
+  console.log(`The driver's name goes first`);
+} else if (hacker2 > hacker1) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?`);
+} */
+
+function lexico (name1, name2) {
+  if (name1 > name2) {
+    console.log(`The driver's name goes first`);
+  } else if (name2 > name1) {
+    console.log(`Yo, the navigator goes first definitely.`);
+  } else {
+    console.log(`What?! You both have the same name?`);
+  }
+}
+
+lexico(hacker1, hacker2);
 
 let mergedStrings = `${hacker1} ${hacker2}`;
 let array = mergedStrings.split(' ');
